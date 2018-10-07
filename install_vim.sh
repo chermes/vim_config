e30165e970b80872a7469c2f372031ad861c4f56
chermes <hermes@hausmilbe.net>
2018-10-07 13:56:18 +0200

    Went back to ultisnips usage in the installation script

--- install_vim.sh
+++ install_vim.sh
@@ -19,10 +19,10 @@ git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/vim-airline/vim-airline
 
 # snippet manager + templates
-# git clone https://github.com/SirVer/ultisnips.git
-git clone https://github.com/tomtom/tlib_vim.git
-git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
-git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/SirVer/ultisnips.git
+# git clone https://github.com/tomtom/tlib_vim.git
+# git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+# git clone https://github.com/garbas/vim-snipmate.git
 #
 git clone git://github.com/honza/vim-snippets.git
 
